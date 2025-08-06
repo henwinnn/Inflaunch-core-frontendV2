907d6a3947f8802f4375fd32c33c7f2454ed24a6
henwinnn <93514124+henwinnn@users.noreply.github.com>
2025-08-06 21:15:47 +0700

    update google secret

--- src/config/googleOAuthConfig.tsx
+++ src/config/googleOAuthConfig.tsx
@@ -1,10 +1,10 @@
 // Configure for Google OAuth 2.0
 export const googleOAuthConfig = {
   clientId: (import.meta as any).env.VITE_GOOGLE_CLIENT_ID, // BUKAN process.env.NEXT_PUBLIC_CLIENT_ID
-  clientSecret: (import.meta as any).env.VITE_GOOGLE_CLIENT_SC,
+  clientSecret: (import.meta as any).env.VITE_GOOGLE_CLIENT_SECRET,
   authorizationUrl: "https://accounts.google.com/o/oauth2/v2/auth",
   tokenUrl: "https://oauth2.googleapis.com/token",
-  redirectUri: "https://inflaunch-core.vercel.app/explore",
+  redirectUri: "https://inflaunch.core.vercel.app/explore",
   scope: "https://www.googleapis.com/auth/youtube.readonly",
   responseType: "code",
 };
